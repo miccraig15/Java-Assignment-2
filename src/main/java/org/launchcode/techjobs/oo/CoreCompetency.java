bfa73894e4658c53d30540e53a59397b3ebe572a
miccraig15 <miccraig15@gmail.com>
2023-02-06 14:27:09 -0600

    Initial Commit

--- src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
+++ src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
@@ -41,4 +41,12 @@ public class CoreCompetency {
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
     //  ONLY a getter for the 'id' field.
 
+    //Getters and Setters:
+
+    public int getId() { return id; }
+
+    public String getValue() { return value; }
+
+    public void setValue(String value) { this.value = value; }
+
 }
